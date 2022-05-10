ccec0c945dd00cff658d97313468ea1a4a0075b8
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-10 20:18:12 +0200

    feat: added darkmode support

--- components/AccentTitle.js
+++ components/AccentTitle.js
@@ -3,7 +3,7 @@ export default function AccentTitle({ title }) {
     <div className="relative">
       <svg
         viewBox="0 0 110 110"
-        class="absolute left-1 -top-1/4 -translate-x-1/2 h-8 w-8 text-accent absolute"
+        class="absolute left-1 -top-1/4 -translate-x-1/2 h-8 w-8 text-accent dark:text-accentdark absolute"
       >
         <circle
           stroke="currentColor"
@@ -14,7 +14,7 @@ export default function AccentTitle({ title }) {
           fill-opacity={0}
         />
       </svg>
-      <div className=" text-3xl font-bold font-Oswald text-primarywhite isolate bg-primaryblack p-0">
+      <div className=" text-3xl font-bold font-Oswald text-primaryblack dark:text-primarywhite isolate bg-primarywhite dark:bg-primaryblack p-0">
         {title}
       </div>
     </div>
