8ad945e7307fa7b4abbf9ffeac1d6f27914902a9
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-10 20:19:05 +0200

    feat: added darkmode support

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className="flex  justify-center">
         <svg
           viewBox="0 0 110 110"
-          class="absolute left-8 -top-5/4 -translate-x-1/4 h-40 w-40 text-accent absolute"
+          class="absolute left-8 -top-5/4 -translate-x-1/4 h-40 w-40 text-accent dark:text-accentdark absolute"
         >
           <circle
             stroke="currentColor"
@@ -18,7 +18,7 @@ export default function Hero() {
           />
         </svg>
         <Image src="/Tobias-Cutout 1.png" width={142} height={151} />
-        <div className="flex flex-col justify-end text-primarywhite">
+        <div className="flex flex-col justify-end text-primaryblack dark:text-primarywhite">
           <div className="ml-4 font-Oswald text-3xl font-bold">
             <div className="">TOBIAS</div>
             <div className="ml-herospc -mt-2">BERGSTRÖM</div>
