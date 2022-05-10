2ee609af07d464a382ef341e2c4136fd9bbd45a7
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-10 20:19:20 +0200

    feat: added darkmode support

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,18 +3,60 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <div className="flex justify-center">
-      <nav className="bg-primaryblack bg-opacity-75 backdrop-blur-sm p-5 fixed w-full max-w-3xl">
+      <nav className="bg-primarywhite/[0.75] backdrop-blur-sm p-5 fixed w-full max-w-3xl dark:bg-primaryblack/[0.75]">
         <div>
           <div className="flex justify-between">
             <div>
               <Image src="/Logo.svg" width="50" height="50" />
             </div>
             <div className="flex">
-              <div className="mr-1">
-                <Image src="/MODE-BUTTON.svg" width="50" height="50" />
-              </div>
+              <button className="w-[50px] h-[50px] mr-1  border-primarywhite stroke-primarywhite border-2 justify-center items-center rounded-full hidden dark:flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                </svg>
+              </button>
 
-              <Image src="/BURGER-BUTTON.svg" width="50" height="50" />
+              <button className="w-[50px] h-[50px] mr-1 border-primaryblack stroke-primaryblack border-2 flex justify-center items-center rounded-full dark:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                </svg>
+              </button>
+
+              <button className="w-[50px] h-[50px] border-primaryblack stroke-primaryblack dark:border-primarywhite dark:stroke-primarywhite border-2 flex justify-center items-center rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
