5d0525c912ddb9f70bb5205512c5aad51f103ab5
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-10 20:18:52 +0200

    fix: attempted fix at visuals

--- components/ExperienceList.js
+++ components/ExperienceList.js
@@ -1,28 +1,29 @@
 export default function ExperienceList() {
   return (
     <div className="relative">
-      <div className="flex flex-row justify-between text-primarywhite font-Oswald font-normal">
+      <div className="flex flex-row justify-between text-primaryblack dark:text-primarywhite font-Oswald font-normal">
         <div>
           <div>ACTIVE</div>
           <div>OLD 2</div>
           <div>OLD 1</div>
         </div>
-        <div className="w-full flex flex-col justify-center">
-          <div>
-            <svg
-              viewBox="0 0 1 1"
-              width="100%"
-              height="24"
-              className=" fill-accent"
-            >
-              <rect width="90%" height="1px" />
-            </svg>
-          </div>
+        <div className="w-full flex flex-col justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-[100%] stroke-accent dark:stroke-accentdark"
+            width="100%"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 12H6" />
+          </svg>
           <div>LINE 2</div>
           <div>LINE 1</div>
         </div>
         <div className="text-center">
-          <div className=" border-accent border-2 rounded-full px-2 text-accent">
+          <div className=" border-accent dark:border-r-accentdark border-2 rounded-full px-2 text-accent dark:text-accentdark">
             NOW
           </div>
           <div>YEAR 2</div>
