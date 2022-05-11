2d779687c8a67831727bbc4d891252da74e7955b
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-11 17:02:19 +0200

    fix: tweaked animation and added margin

--- components/WorksCard.js
+++ components/WorksCard.js
@@ -2,20 +2,22 @@ import Image from "next/image";
 
 export default function WorksCard({ title, content, alt, path }) {
   return (
-    <div className="group justify-center flex-col text-center my-5 w-[350]">
-      <Image
-        className="rounded-[40px]"
-        alt={alt}
-        src={path}
-        width={350}
-        height={184}
-      />
-      <div className="opacity-0 scale-50 -translate-y-8 group-hover:opacity-100 group-hover:translate-y-0 group-hover:scale-100 transition-all duration-300 ease-out">
-        <div className="font-bold text-accent dark:text-accentdark font-Oswald">
-          {title}
-        </div>
-        <div className="font-Oxygen text-primaryblack dark:text-primarywhite">
-          {content}
+    <div className="relative w-[350px] m-2">
+      <div className="group justify-center flex-col text-center">
+        <Image
+          className="rounded-[40px]"
+          alt={alt}
+          src={path}
+          width={350}
+          height={184}
+        />
+        <div className="opacity-0 scale-50 -translate-y-8 group-hover:opacity-100 group-hover:translate-y-0 group-hover:scale-100 transition-all duration-200 ease-out">
+          <div className="font-bold text-accent dark:text-accentdark font-Oswald">
+            {title}
+          </div>
+          <div className="font-Oxygen text-primaryblack dark:text-primarywhite">
+            {content}
+          </div>
         </div>
       </div>
     </div>
