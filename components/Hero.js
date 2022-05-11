503e115b796909280ee9a3728c26e29116c34e88
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-11 18:55:59 +0200

    feat: added responsiveness still not fully functional

--- components/Hero.js
+++ components/Hero.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="relative">
-      <div className="flex  justify-center">
+    <div className="relative ">
+      <div className="flex  justify-center w-full max-w-3xl">
         <svg
           viewBox="0 0 110 110"
           class="absolute left-8 -top-5/4 -translate-x-1/4 h-40 w-40 text-accent dark:text-accentdark absolute"
