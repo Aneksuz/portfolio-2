fb008f4cf17f483e2ae1d35dbaf4f2461670f24c
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-15 22:39:53 +0200

    fix: fixed formatting

--- components/ExperienceList.js
+++ components/ExperienceList.js
@@ -3,15 +3,15 @@ import ExperienceItem from "./ExperienceItem";
 export default function ExperienceList() {
   return (
     <div className="relative">
-      <div className="flex flex-col text-primaryblack dark:text-primarywhite font-Oswald font-normal text-center w-full max-w-4xl">
+      <div className="flex flex-col flex-shrink-0 text-primaryblack dark:text-primarywhite font-Oswald font-normal text-center w-full max-w-4xl">
         <ExperienceItem title={"Umeå University"} year={""} styled={true} />
         <ExperienceItem
-          title={"Avero Customer Service"}
+          title={"Avero, Customer Service"}
           year={"2019"}
           styled={false}
         />
         <ExperienceItem
-          title={"Komplett Customer Service"}
+          title={"Komplett, Customer Service"}
           year={"2018"}
           styled={false}
         />
