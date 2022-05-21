4b7175879f5c4275d6bad2d89f1b6dbae764433e
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-21 18:52:28 +0200

    fix: added more experience items into the list

--- components/ExperienceList.js
+++ components/ExperienceList.js
@@ -3,18 +3,33 @@ import ExperienceItem from "./ExperienceItem";
 export default function ExperienceList() {
   return (
     <div className="relative">
-      <div className="flex flex-col flex-shrink-0 text-primaryblack dark:text-primarywhite font-Oswald font-normal text-center w-full max-w-4xl">
-        <ExperienceItem title={"Umeå University"} year={""} styled={true} />
+      <div className="flex w-full max-w-4xl flex-shrink-0 flex-col text-center font-Oswald font-normal text-primaryblack dark:text-primarywhite">
         <ExperienceItem
-          title={"Avero, Customer Service"}
+          title={"Student, Umeå University"}
+          year={""}
+          styled={true}
+        />
+        <ExperienceItem
+          title={"Buddy Leader, Umeå University"}
+          year={"2021"}
+          styled={false}
+        />
+        <ExperienceItem
+          title={"Customer Service, Avero"}
           year={"2019"}
           styled={false}
         />
         <ExperienceItem
-          title={"Komplett, Customer Service"}
+          title={"Customer Service, Komplett"}
           year={"2018"}
           styled={false}
         />
+        <ExperienceItem
+          title={"Customer Service, SYKES Sweden"}
+          year={"2018"}
+          styled={false}
+        />
+        <ExperienceItem title={"Intern, AFRY"} year={"2017"} styled={false} />
       </div>
     </div>
   );
